fd496f365f39f6764217fcd61703d9faa958ceae
Capsalt <hbasri2304@gmail.com>
2022-02-04 12:53:38 +0300

    v4

--- src/main/java/com/prorent/carrental/repository/UserRepository.java
+++ src/main/java/com/prorent/carrental/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.prorent.carrental.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.prorent.carrental.domain.User;
+import com.prorent.carrental.exception.ConflictException;
+import com.prorent.carrental.exception.ResourceNotFoundException;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email) throws ResourceNotFoundException;
 
+	Boolean existsByEmail(String email) throws ConflictException;
 }
---
+++ src/main/java/com/prorent/carrental/security/AuthTokenFilter.java
@@ -0,0 +1,9 @@
+package com.prorent.carrental.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+	
+	
+
+}
