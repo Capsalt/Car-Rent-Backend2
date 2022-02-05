29b0225a986ba5e7edab241384a27ae5d27d6e1f
Capsalt <hbasri2304@gmail.com>
2022-02-05 14:49:30 +0300

    security

---
+++ src/main/java/com/prorent/carrental/security/config/WebSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.prorent.carrental.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.prorent.carrental.security.AuthEntryPointJwt;
+import com.prorent.carrental.security.AuthTokenFilter;
+import com.prorent.carrental.security.service.UserDetailsServiceImpl;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+private UserDetailsServiceImpl userDetailsServiceImpl;
+	
+	private final AuthEntryPointJwt unauthorizedHandler;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean 
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
+	} 
+	
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception{
+		return super.authenticationManager();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().cors().disable()
+		.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.authorizeHttpRequests().antMatchers("/register","/login").permitAll()
+		.anyRequest().authenticated();
+		
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+	}
+
+}
