89d90a7ab23b7bfc59cf478adb9006c1f1773df6
Capsalt <hbasri2304@gmail.com>
2022-02-08 20:16:28 +0300

    car

--- src/main/java/com/prorent/carrental/controller/ImageFileController.java
+++ src/main/java/com/prorent/carrental/controller/ImageFileController.java
@@ -1,4 +1,4 @@
-package com.prorent.carrental.domain;
+package com.prorent.carrental.controller;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.prorent.carrental.domain.ImageFile;
 import com.prorent.carrental.exception.UploadImageException;
 import com.prorent.carrental.service.ImageFileService;
 import com.prorent.carrental.service.dto.ImageFileDTO;
